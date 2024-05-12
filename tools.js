08773bdfce23b6f9decf5ce7e07961f38cf84887
Thandeka <jcw80040@gmail.com>
2024-05-12 00:21:35 -0400

    Optimized code, added another tool

--- tools.js
+++ tools.js
@@ -1,72 +1,72 @@
-if ("token" in localStorage) {
-  // User is logged in, do nothing
-} else {
+if (!localStorage.getItem("token")) {
   window.location.href = "./login.html";
 }
 
-let token = localStorage.getItem("token");
+const token = localStorage.getItem("token");
 
-let headers = {
+const headers = {
   Authorization: token,
-  "x-super-properties":
-    "ewogICJvcyI6ICJXaW5kb3dzIiwKICAiY2xpZW50X2J1aWxkX251bWJlciI6IDE1MjQ1MAp9",
+  "x-super-properties": "ewogICJvcyI6ICJXaW5kb3dzIiwKICAiY2xpZW50X2J1aWxkX251bWJlciI6IDE1MjQ1MAp9",
 };
 
-function getGuildCount() {
-  fetch("https://discord.com/api/v10/users/@me/guilds", { headers })
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        if (response.status === 429) {
-        Toastify({
-          text: "Ratelimited!",
-          duration: 3000,
-          newWindow: true,
-          gravity: "top",
-          position: "right", 
-          stopOnFocus: true,
-          style: {
-            background: "#e73131",
-            color: "#ffffff",
-          }
-        }).showToast();} else {
-          Toastify({
-            text: "An error occurred.",
-            duration: 3000,
-            newWindow: true,
-            gravity: "top",
-            position: "right", 
-            stopOnFocus: true,
-            style: {
-              background: "#e73131",
-              color: "#ffffff",
-            }
-          }).showToast();
-        }
-      }
-    })
-    .then((data) => {
-      let guild_count = data.filter((item) => item.hasOwnProperty("id")).length;
-      Toastify({
-        text: "Servers: " + guild_count,
-        duration: 3000,
-        newWindow: true,
-        gravity: "top",
-        position: "right", 
-        stopOnFocus: true,
-        style: {
-          background: "#111c1d",
-          color: "#e2fcfa",
-        }
-      }).showToast();
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-}
+const showToast = (message, background, color) => {
+  Toastify({
+    text: message,
+    duration: 3000,
+    newWindow: true,
+    gravity: "top",
+    position: "right",
+    stopOnFocus: true,
+    style: {
+      background: background,
+      color: color,
+    },
+  }).showToast();
+};
 
-function logout() {
+const handleErrors = async (response) => {
+  if (!response.ok) {
+    if (response.status === 429) {
+      showToast("Ratelimited!", "#e73131", "#ffffff");
+    } else {
+      showToast("An error occurred.", "#e73131", "#ffffff");
+    }
+    throw Error(response.statusText);
+  }
+  return response;
+};
+
+const getGuildCount = async () => {
+  try {
+    const response = await fetch("https://discord.com/api/v10/users/@me/guilds", { headers });
+    await handleErrors(response);
+    const data = await response.json();
+    const guild_count = data.filter((item) => item.hasOwnProperty("id")).length;
+    showToast("Servers: " + guild_count, "#111c1d", "#e2fcfa");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const countFriends = async () => {
+  try {
+    const response = await fetch("https://discord.com/api/v9/users/@me/relationships", { headers });
+    await handleErrors(response);
+    const data = await response.json();
+    let typeOneCount = 0;
+    data.forEach((relationship) => {
+      if (relationship.type === 1) {
+        typeOneCount++;
+      }
+    });
+    showToast("Friends: " + typeOneCount, "#111c1d", "#e2fcfa");
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+};
+
+
+const logout = () => {
   localStorage.removeItem("token");
-  window.location.reload()
-}
+  window.location.reload();
+};
