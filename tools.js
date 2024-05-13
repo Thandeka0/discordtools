131574ab28cd9ed063ed768b3756fcbabc6c2cd5
Thandeka <jcw80040@gmail.com>
2024-05-12 22:44:19 -0400

    Add more relationship info, make logo smaller

--- tools.js
+++ tools.js
@@ -48,22 +48,37 @@ const getGuildCount = async () => {
   }
 };
 
-const countFriends = async () => {
+const countRelationships = async () => {
   try {
     const response = await fetch("https://discord.com/api/v9/users/@me/relationships", { headers });
     await handleErrors(response);
     const data = await response.json();
-    let typeOneCount = 0;
-    data.forEach((relationship) => {
+    let friends = 0, blocked = 0, ofr = 0, ifr = 0;
+
+    for (const relationship of data) {
       if (relationship.type === 1) {
-        typeOneCount++;
+        friends++;
+      } else if (relationship.type === 2) {
+        blocked++;
+      } else if (relationship.type === 3) {
+        ifr++;
+      } else if (relationship.type === 4) {
+        ofr++;
       }
-    });
-    showToast("Friends: " + typeOneCount, "#111c1d", "#e2fcfa");
+    }
+
+    const info = `
+    Friends: ${friends}⠀Outgoing friend requests: ${ofr}<br>
+    Blocked: ${blocked}⠀Incoming friend requests: ${ifr}
+    `;
+
+    document.getElementById('relationship-info').innerHTML = info
+
   } catch (error) {
     console.error("Error:", error.message);
   }
 };
+
 
 
 const logout = () => {
